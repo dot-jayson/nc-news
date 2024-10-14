12812aee4175350af8187ee520a1e4d1dce68e3c
Jayson Chiu <jaysonchiu@Jaysons-MacBook-Air.local>
2024-10-14 14:02:15 +0100

    added error handling for task 4

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,9 +1,13 @@
 const { fetchArticleById } = require("../models/articles.models");
 
-function getArticleById(request, response) {
-  fetchArticleById(request.params.article_id).then((article) => {
-    response.status(200).send({ article: article.rows });
-  });
+function getArticleById(request, response, next) {
+  fetchArticleById(request.params.article_id)
+    .then((article) => {
+      response.status(200).send({ article: article.rows });
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
 module.exports = { getArticleById };
--- models/articles.models.js
+++ models/articles.models.js
@@ -4,6 +4,9 @@ function fetchArticleById(id) {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [id])
     .then((article) => {
+      if (article.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Article does not exist" });
+      }
       return article;
     });
 }
